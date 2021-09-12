2e1c02ea11953d8f5c420f0726f0ccd88c33b2e9
Deathstroke751 <48074616+Deathstroke751@users.noreply.github.com>
2021-09-12 08:53:25 +0530

    Update presearch.bat

--- presearch.bat
+++ presearch.bat
@@ -1,2 +1,2 @@
-curl https://github.com/Deathstroke751/AutoPresearch/releases/download/eternal/presearch.py -o presearch.py
+curl -LJO https://github.com/Deathstroke751/AutoPresearch/releases/download/eternal/presearch.py -o presearch.py
 python presearch.py
