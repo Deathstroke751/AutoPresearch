791c7ed4bdb02d389c5b90ca7c4a277d3fbd793c
Deathstroke751 <48074616+Deathstroke751@users.noreply.github.com>
2021-09-12 08:26:45 +0530

    Update presearch.bat

--- presearch.bat
+++ presearch.bat
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/presearch.py?token=ALOY66DQGOP4E7WOYY74ELTBHVWFC
+curl https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/presearch.py?token=ALOY66DQGOP4E7WOYY74ELTBHVWFC -o presearch.py
 python presearch.py
