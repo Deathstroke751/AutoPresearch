e310e67d8972f0864821ab1f59fbad51ee171014
Deathstroke751 <48074616+Deathstroke751@users.noreply.github.com>
2021-09-15 17:17:48 +0530

    Update test-windows.bat

--- test-windows.bat
+++ test-windows.bat
@@ -1,3 +1,3 @@
-del presearch.py
+del test-presearch.py
 curl -LJO https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/test-presearch.py -o test-presearch.py
-python presearch.py
+python test-presearch.py
