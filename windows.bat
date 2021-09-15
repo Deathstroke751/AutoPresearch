f20aa4fbc6ac2499790db1862259338083277bd6
Deathstroke751 <48074616+Deathstroke751@users.noreply.github.com>
2021-09-15 17:08:36 +0530

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,3 +1,3 @@
 del presearch.py
-curl -LJO https://github.com/Deathstroke751/AutoPresearch/releases/download/eternal/presearch.py -o presearch.py
+curl -LJO https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/presearch.py -o presearch.py
 python presearch.py
