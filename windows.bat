f15c5349bcf2bea1406d91d6a49f44787b18d177
Deathstroke751 <48074616+Deathstroke751@users.noreply.github.com>
2021-09-15 18:45:59 +0530

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,4 +1,4 @@
 del presearch.py
 timeout 3
-curl -LJO https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/presearch.py -o presearch.py
+curl https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/presearch.py -o presearch.py
 python presearch.py
