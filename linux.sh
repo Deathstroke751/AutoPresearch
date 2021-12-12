344f6eabd7849a0657ed7fb414eda7699033f37f
Deathstroke751 <mightyroyalgiant@gmail.com>
2021-12-12 10:21:16 +0530

    m

--- linux.sh
+++
@@ -1,4 +0,0 @@
-rm presearch.py
-sleep 2
-wget https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/presearch.py
-python3 presearch.py
--- test-windows.bat
+++
@@ -1,4 +0,0 @@
-del test-presearch.py
-timeout 3
-curl https://raw.githubusercontent.com/Deathstroke751/AutoPresearch/main/test-presearch.py -o test-presearch.py
-python test-presearch.py
